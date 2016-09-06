b46bd5175e772bb5eef6f4ca3fcf7fd9c470a331
2n2n <ayolachll@gmail.com>
2016-09-06 18:07:09 +0800

    initial commit

---
+++ app/app.js
@@ -0,0 +1,4 @@
+var app = angular.module('testApp', [])
+.controller('mainController', function($scope) {
+    $scope.message = 'Hello';
+});
---
+++ app/directives/datagrid.js
@@ -0,0 +1,6 @@
+app.directive('test', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/views/datagrid.html'
+    };
+})
